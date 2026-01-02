78bffe6b52f69e7215e77e21c58fc87a349be82b
Daniel Steman <daniel-steman@live.nl>
2026-01-02 13:31:00 +0100

    add home route

--- internal/rest/router.go
+++ internal/rest/router.go
@@ -1,9 +1,30 @@
 package rest
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func NewRouter() http.Handler {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/", handleHome)
 	mux.HandleFunc("/users", handleUsers)
 	return JSONMiddleware(mux)
+}
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	response := map[string]interface{}{
+		"message": "Welcome to the Go REST API",
+		"version": "1.0.0",
+		"endpoints": map[string]string{
+			"users": "/users",
+		},
+	}
+
+	json.NewEncoder(w).Encode(response)
 }
